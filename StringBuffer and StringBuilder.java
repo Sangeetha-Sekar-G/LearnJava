d9a417faaa1a98cb9da364ffb5eb90c5a3dbb478
Sangeetha-Sekar-G <sangeetha0899@gmail.com>
2025-05-13 16:47:57 +0200

    Update StringBuffer and StringBuilder.java

--- StringBuffer and StringBuilder.java
+++ StringBuffer and StringBuilder.java
@@ -22,3 +22,21 @@ class Demo {
 	
 	}
 }
+
+
+// Mutable vs Immutable string.java
+
+// public class Demo {
+// 	public static void main(String[] args) 
+// 	{
+// 		String name="navin";
+// 		name=name+"reddy";
+// 		System.out.println("hello"+name);
+		
+// 		String s1="Navin";
+// 		String s2="Navin";
+		
+// 		System.out.println(s1==s2);	
+		
+// 	}
+// }
