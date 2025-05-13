335a29e756b3675cb348b5583d4413a8d74d9e62
Sangeetha-Sekar-G <sangeetha0899@gmail.com>
2025-05-13 16:53:15 +0200

    Update Lambda Expression.java

--- Lambda Expression.java
+++ Lambda Expression.java
@@ -35,3 +35,32 @@ public class Demo {
     }
 }
 
+
+//Lambda Expression with return statement.java
+/*@FunctionalInterface
+interface A
+{
+	int add(int i, int j);
+}
+
+
+public class Demo {
+    public static void main(String[] args) {
+//    	
+//   	A obj=new A()
+//   	{
+//   		public int add(int i, int j)
+//   		{
+//   			return i+j;
+//   		}
+//   	};
+    	
+    	A obj=(i,j) -> i+j;
+   	
+   	int result=obj.add(5, 4);
+   	System.out.println(result);
+    }
+}
+*/
+
+
