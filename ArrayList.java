82c37b7801f57e6c66d2db68721da9ba3e7d4ebd
Sangeetha-Sekar-G <sangeetha0899@gmail.com>
2025-06-16 19:10:11 +0200

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -3,6 +3,13 @@ Collection -> Interface
 Collections -> classe with multiple methods
 			    different type of data structures
 */
+/*
+Drawbacks:-
+- The memory allocation is contiguous.
+- The size of an array is fixed. Array size cannot be expand.
+- Searching takes time.
+- Array can store values of only same type. It can store homogeneous type value only.
+*/
 
 
 import java.util.ArrayList;
